4669b318e16da5755a1dd87454fec9edcc86c5c3
Spencer Hayes <spencer.hayes@stu.ocsb.ca>
2022-11-23 14:03:57 -0500

    Formatted Code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
 a = randint(1,100)
 b = randint(1,100)
+
 userAnswer = input("What is " + str(a) + " + " + str(b) + "\n")
+
 if int(userAnswer) == a+b:
   print("\nCorrect!\n")
 else:
